8bfb5c078035b98b4cd6861a83599bdc318f468d
Kevin Salimi <kevin.sallimi@gmail.com>
2022-06-02 00:36:34 +0200

    remove yml files

--- Startup.cs
+++ Startup.cs
@@ -20,14 +20,12 @@ namespace K1_Static_Website
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllers();
             services.AddMvc();
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
@@ -37,13 +35,11 @@ namespace K1_Static_Website
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
             app.UseStaticFiles();
 
-            // the following enables MVC and Razor Pages
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
